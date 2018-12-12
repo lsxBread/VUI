6ec85d650fedbf232a81853010d3b43e61b75e26
Shixun Liu <shixun.liu1023@gmail.com>
2018-12-13 08:53:44 +1100

    create ButtonGroup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './Icon'
+import ButtonGroupp from './ButtonGroup'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroupp)
 
 new Vue({
   el: '#app',
