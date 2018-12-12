50cb2b585642488c1cc3939eaf1fad4ad44c49b6
Shixun Liu <shixun.liu1023@gmail.com>
2018-12-13 08:59:27 +1100

    check the child element of ButtonGroup

--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -4,7 +4,18 @@
   </div>
 </template>
 
-<script></script>
+<script>
+  export default {
+    mounted () {
+      for (let node of this.$el.children) {
+        let name = node.nodeName.toLowerCase()
+        if (name !== 'button') {
+          console.warn(`g-button-group 的子元素应该都是g-button，但是你写的是 ${name}`)
+        }
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
   .g-button-group {
