7f750499ee1da06af1a9e9a00363152783ab241a
Shixun Liu <shixun.liu1023@gmail.com>
2018-12-13 15:27:07 +1100

    fix unit testing for input component

--- test/input.test.js
+++ test/input.test.js
@@ -73,10 +73,18 @@ describe('Input', () => {
         vm = new Constructor({}).$mount()
         const callback = sinon.fake()
         vm.$on(eventName, callback)
-        const event = new Event(eventName)
+        let event = new Event(eventName)
+        Object.defineProperty(
+          event,
+          'target',
+          {
+            value: {value: 'hi'},
+            enumerable: true
+          }
+        )
         let inputElement = vm.$el.querySelector('input')
         inputElement.dispatchEvent(event)
-        expect(callback).to.have.been.calledWith(event)
+        expect(callback).to.have.been.calledWith('hi')
       })
     })
   })
