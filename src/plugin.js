d6c40d6a883ec0142c942aa2136a527f32e4d67c
Shixun Liu <shixun.liu1023@gmail.com>
2018-12-15 17:44:02 +1100

    only popup a single toast one time

--- src/plugin.js
+++ src/plugin.js
@@ -1,14 +1,24 @@
 import Toast from './Toast'
+
+let curretToast
+
 export default {
   install (Vue, options) {
     Vue.prototype.$toast = (message, toastOptions) => {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions 
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      if (curretToast) {
+        curretToast.close()
+      }
+      curretToast = createToast({ Vue, message, propsData: toastOptions})
     }
   }
+}
+
+// helpers
+function createToast ({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({ propsData })
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
 }
